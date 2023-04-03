fdda41521723d3e185f57ea9ee2b16d5d5dfac66
jerryq0101 <jerryq0101@gmail.com>
2023-04-03 15:51:05 -0700

    First page responsiveness fixes

--- src/pages/First-Page.js
+++ src/pages/First-Page.js
@@ -16,7 +16,7 @@ export default function FirstPage() {
         )
     }
     return (
-        <div className="w-full h-screen bg-cover bg-no-repeat relative flex ">
+        <div className="w-full min-h-screen bg-cover bg-no-repeat relative flex ">
             <Image alt="blurred colors background" src={redgreenblue} layout="intrinsic" objectFit="cover" objectPosition="center" className="z-10 " />
             <div className="max-w-screen-2xl absolute inset-0 z-10 mx-auto text-center">
                 <div className="px-5 py-5 flex flex-col z-0">
@@ -24,25 +24,25 @@ export default function FirstPage() {
                         <div className="h-16 text-black flex flex-row items-center justify-between">
                             {/* Header */}
                             <img alt="RFDN Logo" src="/RFDN.svg" height="21"/>
-                            <div id="firstpage-scroll" className="flex gap-6">
+                            <div id="firstpage-scroll" className="hidden sm:block flex gap-6">
                                 <span><a href="mailto:info@refoundation.co">Contact</a></span>
                                 <span><a href="https://build23.xyz" target="_blank">Our Past Iteration</a></span>
                             </div>
                             <div className="cursor-pointer hover:bg-blue-400 focus:outline-none px-3 py-2 bg-black rounded-lg">
-                                <button className="text-white ">
+                                <button className="text-white">
                                     Work with us
                                 </button>
                             </div>
                         </div>
                         {/* The tagline and the svg  */}
                         <div className="flex flex-1 ">
-                            <div className="text-8xl font-bold font-SuisseIntl self-center px-5">
+                            <div className="text-5xl md:text-8xl font-bold font-SuisseIntl self-center px-5">
                                 We are a 501(c)(3) non-profit that creates hackathon opportunities for youth.
                             </div>
                         </div>
                         <div className="justify-self-end">
-                            <div className="flex flex-row gap-4 justify-center ">
-                                <button  className="cursor-pointer hover:bg-blue-400 focus:outline-none px-3 py-2 bg-black rounded-lg text-white">
+                            <div className="flex flex-row gap-4 justify-center">
+                                <button className="cursor-pointer hover:bg-blue-400 focus:outline-none px-3 py-2 bg-black rounded-lg text-white">
                                     <ScrollButton />
                                 </button>
                             </div>
